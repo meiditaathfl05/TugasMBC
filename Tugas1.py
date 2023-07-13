54e46ff740f082575eeddec97a0d58cbd77f6c9c
meiditaathfl05 <meiditaathifal@icloud.com>
2023-07-13 12:34:08 +0700

    menghapus sistem inap

--- Tugas1.py
+++ Tugas1.py
@@ -1,19 +1,18 @@
-def tarifParkir(waktuParkir):
-    tarifParkir = 5000
-    masuk = input ("Waktu Masuk Parkir")
-    keluar = input ("Waktu Keluar Parkir")
+def tarifParkir():
+    masuk = float(input ("Waktu Masuk Parkir : "))
+    keluar = float(input ("Waktu Keluar Parkir : "))
     durasi = keluar - masuk
-    bayar 
+    bayar = float
                
     print ("====Tarif Parkir====")
     print("Waktu Masuk : ", masuk)
-    print ("Waktu Masuk ", keluar)
+    print ("Waktu Keluar ", keluar)
     if keluar > masuk and keluar - masuk > 2:
-        durasi = keluar-2-masuk 
-        bayar = tarifParkir + (2000*durasi)
-    elif keluar < masuk  and keluar - masuk < 2:
-        bayar = 5000
-    else :
-        durasi = 24 - masuk - 2 - keluar
+        durasi = keluar - 2 - masuk 
         bayar = 5000 + (2000*durasi)
+    else :
+        keluar < masuk and keluar - masuk < 2
+        bayar = 5000
+
     print ("Biaya Parkir : Rp ", bayar)
+tarifParkir()
